4140af3805434d782f9b74884bb6008b4a2af0e4
Kunal Dixit <contactkunaldixit@gmail.com>
2022-05-27 13:45:31 +0800

    Updated

--- 5.1_php_File_Structure/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-include"functions.php";
-require_once"includes/header.php";
-$title = "Hello, World";
-// require "views/index.view.php";
-view("index",$title);
-require_once "includes/footer.php"
-?>
--- Include/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-const USER_NAME = "admin@admin.com";          //setting up a constant - observe no $ sign being prefixed 
-const USER_PASSWORD = "Mother@1234";
-?>
-
-<!-- Setting 2 constants in this config.php file which'll be used later when we'll compare user's feed with these to authenticate users. -->
